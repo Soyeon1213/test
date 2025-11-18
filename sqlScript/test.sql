b8901e125aa0a05c0791c36374fc981e12022518
Soyeon <80968866+Soyeon1213@users.noreply.github.com>
2025-11-18 15:01:33 +1100

    Refactor SQL script for aggregation calculations

--- sqlScript/test.sql
+++ sqlScript/test.sql
@@ -1,19 +1,26 @@
 WITH base AS (
     SELECT
         id,
-        id % 10            AS grp,
-        id * id            AS squared
-    FROM range(0, 30000000)
+        id % 50              AS grp,
+        id * 3 + 7           AS v1,
+        id * id              AS v2
+    FROM range(0, 80000000) 
 ),
-agg AS (
+level1 AS (
     SELECT
         grp,
-        COUNT(*)           AS cnt,
-        AVG(squared)       AS avg_sq,
-        MAX(squared)       AS max_sq
+        COUNT(*)             AS cnt,
+        AVG(v1)              AS avg_v1,
+        AVG(v2)              AS avg_v2
     FROM base
     GROUP BY grp
+),
+level2 AS (
+    SELECT
+        SUM(cnt)             AS total_cnt,
+        AVG(avg_v1)          AS global_avg_v1,
+        AVG(avg_v2)          AS global_avg_v2
+    FROM level1
 )
 SELECT *
-FROM agg
-ORDER BY grp;
+FROM level2;
